5eba36fe7a4f0b0008a11111adc38caa57ca9dc5
ayalaluis769 <71358053+ayalaluis769@users.noreply.github.com>
2021-08-24 13:12:24 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import name
 from banking_pkg import account
 
 def atm_menu(name):
@@ -11,30 +12,46 @@ def atm_menu(name):
     print("| 3.    Withdraw    | 4.    Logout       |")
     print("------------------------------------------")
 
-while True:
 
+
+print("          === Automated Teller Machine ===          ")
+name = input("Enter name to register: ")
+pin = input("Enter PIN: ")
+balance = 0
+
+print(name, "has been registered with a starting balance of $" + str(balance))
+
+while True:
+    
     print("          === Automated Teller Machine ===          ")
-    user = input("Enter name to register: ")
-    pin = input("Enter PIN: ")
+    print("\nLOGIN")
+    user_name = input("Enter name: ")
+    user_pin = input("Enter PIN: ")
 
-    balance = 0
-    #print(user, "has been registered with a starting balance of $", balance)
-    name_to_validate = user
-    pin_to_validate = pin
-    if (name_to_validate == user) and (pin_to_validate == pin):
-        print("Login Succesful!")
+    if (user_name == name) and (user_pin == pin):
+        print("Login successful!")
         break
-    elif (name_to_validate != user) and (pin_to_validate == pin):
-        print("Invalid credentials")
     else:
-        print("Invalid credentials") 
-
+        print("Invalid Credentials!")
+    
 while True:
-    atm_menu(user)
+    atm_menu(name)
     option = input("Choose an option: ")
 
     if option == "1":
-        account.show_balance(balance)
+        account.show_balance(balance)    
     elif option == "2":
-        account.deposit(balance) 
-           
+        
+        balance = account.deposit(balance)
+        account.show_balance(balance)
+
+    elif option == "3":
+        
+        balance = account.withdraw(balance)
+        account.show_balance(balance)
+        
+    elif option == "4":       
+       account.logout(name)
+       break 
+    else:
+        print("Wrong Choice!")
